b9c276f4ae8d6058210f04b20245bab4de600af3
Anthony Fu <anthonyfu117@hotmail.com>
2017-03-24 04:08:27 +0800

    Update Readme. Bump version to v0.0.2

--- README.md
+++ README.md
@@ -4,16 +4,27 @@ FaceOnly is a plugin for the [Hearthstone Deck Tracker](https://github.com/Heart
 FaceOnly covers your opponent's minions so you can focus on **FACE**!!
 It's just a toy for face decks, inspired from [this reddit](https://www.reddit.com/r/hearthstone/comments/30b5dn/how_to_play_face_hunter_properly/).
 
+* **Face Decks**
+Works best with face decks.
+
+* **Taunt**
+**Taunt** minions can be seen with triangle bottom.
+
+* **Toggle**
+Toggle button is just next to your opponent's hero.
+
 ![demo](https://cloud.githubusercontent.com/assets/11247099/24262731/f0b0e1d0-1035-11e7-8649-c1e2a40c8481.jpg)
-*This is only the demo, forget about the deck.*
+*This is the screenshot of the first version*
 
 ## Install
 1. Download the latest `FaceOnly.dll` from [Releases](https://github.com/antfu/FaceOnly/releases)
 2. Copy the file into `%AppData%\HearthstoneDeckTracker\Plugins`
-3. (Re-)start the HDT and enable FaceOnly in settings.
+3. (Re-)start the HDT
+4. Enable FaceOnly in `Options -> Tracker -> Plugins`.
+
+For more details, refer to [HDT wiki](https://github.com/HearthSim/Hearthstone-Deck-Tracker/wiki/Available-Plugins).
 
 ## Todo
-- Enable/Disable button/menu
 - Color select
 - Opacity adjustment
 - Textures, better looking
--- src/FaceOnly.cs
+++ src/FaceOnly.cs
@@ -4,41 +4,43 @@ using System.Windows.Controls;
 using Hearthstone_Deck_Tracker.API;
 using Hearthstone_Deck_Tracker.Plugins;
 using System.ComponentModel;
-using System.Windows.Data;		 
+using System.Windows.Data;
 
 namespace HDT.Plugins.FaceOnly
 {
-    public class FaceOnly : IPlugin
-    {
-        private Mask mask;
+	public class FaceOnly : IPlugin
+	{
+		private Mask mask;
 		private float opacity = 0.98f;
 		private String colorHex = "#000000";
+		private bool gameStarted = false;
+		private bool mulliganDone = false;
 
 		#region Manifest
 		public string Author
-        {
-            get { return "Anthony Fu"; }
-        }
+		{
+			get { return "Anthony Fu"; }
+		}
 
-        public string ButtonText
-        {
-            get { return "Settings"; }
-        }
+		public string ButtonText
+		{
+			get { return "Settings"; }
+		}
 
-        public string Description
-        {
-            get { return "A funny toy for face attacking decks."; }
-        }					  
+		public string Description
+		{
+			get { return "A funny toy for face attacking decks."; }
+		}
 
-        public string Name
-        {
-            get { return "FaceOnly"; }
-        }
+		public string Name
+		{
+			get { return "FaceOnly"; }
+		}
 
-        public Version Version
-        {
-            get { return new Version(0, 0, 1); }
-        }
+		public Version Version
+		{
+			get { return new Version(0, 0, 2); }
+		}
 		#endregion
 
 		public MenuItem MenuItem
@@ -47,25 +49,32 @@ namespace HDT.Plugins.FaceOnly
 		}
 
 		public void OnButtonPress()
-		{										 
+		{
 		}
 
 		public void OnLoad()
-        {
-            mask = new Mask();
+		{
+			mask = new Mask();
 			mask.Opacity = opacity;
 			mask.Color = colorHex;
-            Core.OverlayCanvas.Children.Add(mask);
+			Core.OverlayCanvas.Children.Add(mask);
 
 			//events													 
-			GameEvents.OnGameStart.Add(mask.GameStart);
+			GameEvents.OnGameStart.Add(() =>
+			{
+				gameStarted = true;
+			});
 			GameEvents.OnTurnStart.Add(a => mask.TurnStart());
-			GameEvents.OnGameEnd.Add(mask.GameEnd);
+			GameEvents.OnGameEnd.Add(() =>
+			{
+				gameStarted = false;
+				mask.GameEnd();
+			});
 
 			Canvas.SetTop(mask, -2);
 			Canvas.SetLeft(mask, 0);
 			Canvas.SetBottom(mask, 0);
-			Canvas.SetRight(mask, 0);					
+			Canvas.SetRight(mask, 0);
 			Canvas.SetZIndex(mask, -1);
 
 			Binding widthBinding = new Binding();
@@ -83,17 +92,36 @@ namespace HDT.Plugins.FaceOnly
 				var size = e.NewSize;
 				mask.Height = size.Height;
 				mask.Width = size.Width;
-			};	  
+			};
+
+			mulliganDone = Core.Game.IsMulliganDone;
+			gameStarted = mulliganDone && !Core.Game.IsInMenu;
+
+			if (!mulliganDone)
+			{
+				mask.GameEnd();
+			}
+			else
+			{
+				mask.GameStart();
+			}
 		}
 
-        public void OnUnload()
-        {
-            Core.OverlayCanvas.Children.Remove(mask);
-        }						   
-
-        public void OnUpdate()
-        {
-			mask.OnUpdate();
+		public void OnUnload()
+		{
+			Core.OverlayCanvas.Children.Remove(mask);
 		}
-    }
+
+		public void OnUpdate()
+		{
+			if (gameStarted)
+			{
+				if (!mulliganDone && Core.Game.IsMulliganDone)
+					mask.GameStart();
+				mulliganDone = Core.Game.IsMulliganDone;
+
+				mask.OnUpdate();
+			}
+		}
+	}
 }
